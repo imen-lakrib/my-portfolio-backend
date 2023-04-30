529d7696299c6b27c01c63859d42f656bf17d8d4
imen-lakrib <lak12imen@gmail.com>
2023-04-30 08:51:06 +0100

    no thing

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.use(cors());
 // file name 
 
 app.use(express.static(path.join(__dirname,'public'), {dotfiles: "allow"}))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
 // routes 
 app.use('/secretpannel', require('./routes/admin'))
 app.use('/blog', require('./routes/blog'))
@@ -34,35 +40,5 @@ app.use('/project', require('./routes/project'))
 app.use('/skill', require('./routes/skill'))
 
 
-// email sending contactform:
-app.post('/send-email', (req, res) => {
-    const { name, email, message } = req.body;
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'your-email-address@gmail.com',
-        pass: 'your-email-password'
-      }
-    });
-    const mailOptions = {
-      from: email,
-      to: 'your-email-address@gmail.com',
-      subject: `New message from ${name}`,
-      html: `<p>Name: ${name}</p>
-      <p>Email: ${email}</p>
-      <p>Message: ${message}</p>`,
-    };
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-        res.send('error');
-      } else {
-        console.log('Email sent: ' + info.response);
-        res.send('success');
-      }
-    });
-  });
-
-  //
 
 app.listen(Port, ()=> console.log(`connect in port ${Port}`))
